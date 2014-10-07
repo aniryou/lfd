47c407eadea18b3b175f395cab9310f6b03d5e6b
aniryou <arpitachoudhary@Arpitas-MacBook-Pro.local>
2014-10-08 01:02:20 +0530

    updated pla

--- pla.py
+++ pla.py
@@ -10,6 +10,11 @@ def sign(x):
 	return 1 if x>=0 else -1
 
 def classify(x,y,slope,intercept):
+	'''
+	classify point (x,y) against line (y = slope*x + intercept)
+	point in region with origin is classified negative
+	point in region without origin is classified positive
+	'''
 	m=y/x
 	x_=intercept/(m-slope)
 	y_=m*x_
@@ -20,15 +25,42 @@ def classify(x,y,slope,intercept):
 	r=linalg.norm(v1)
 	r_=linalg.norm(v2)
 	return sign(r-r_)
+		
+def predict(X,W):
+	'''
+	predict classification of points in X, using perceptron params W
+	'''
+	predictions=[]
+	for v in X:
+		v_=numpy.concatenate(([1], v))
+		predictions.append(sign(numpy.dot(v_,W)))
+	return numpy.array(predictions)
 
-def generate(N, slope, intercept):
+def accuracy(Y_act,Y_pred):
+	'''
+	perceptron accuracy, Y_actual vs Y_predicted
+	'''
+	test=[]
+	for i in range(0,len(Y_act)):
+		test.append(1 if Y_act[i]==Y_pred[i] else 0)
+	return numpy.mean(test)
+
+def generate_data(N, slope, intercept):
+	'''
+	generate N points classified as positive/negative
+	using line defined by slope and intercept
+	'''
 	X=random.uniform(-1.,1.,size=(N,2))
 	Y=numpy.empty(N)
 	for i in range(0,N):
 		Y[i]=classify(*X[i],slope=slope,intercept=intercept)
 	return X,Y
 
-def pla(X,Y):
+def perceptron_learning(X,Y):
+	'''
+	perceptron learning algorithm
+	returns number of iterations, params estimated
+	'''
 	from random import shuffle
 	W=numpy.zeros(3)
 	itercount=0
@@ -47,34 +79,28 @@ def pla(X,Y):
 				W = W - clf*v
 		if(len(misclassified)==0):
 			return itercount, W
-		
-def predict(X,W):
-	predictions=[]
-	for v in X:
-		v_=numpy.concatenate(([1], v))
-		predictions.append(sign(numpy.dot(v_,W)))
-	return numpy.array(predictions)
 
-def accuracy(Y,Y_pred):
-	test=[]
-	for i in range(0,len(Y)):
-		test.append(1 if Y[i]==Y_pred[i] else 0)
-	return numpy.mean(test)
-
-def perceptron_training (x1, y1, x2, y2, training_size):
+def perceptron_training(x1, y1, x2, y2, sample_size):
+	'''
+	train perceptron over given line, for sample_size points
+	'''
 	slope=(y2-y1)/(x2-x1)
 	intercept=y1-slope*x1
-	X,Y=generate(2*training_size,slope,intercept)
-	X_train=X[:training_size]
-	Y_train=Y[:training_size]
-	X_test=X[training_size:]
-	Y_test=Y[training_size:]
-	iter_count,W=pla(X_train,Y_train)
+	X,Y=generate_data(2*sample_size,slope,intercept)
+	X_train=X[:sample_size]
+	Y_train=Y[:sample_size]
+	X_test=X[sample_size:]
+	Y_test=Y[sample_size:]
+	iter_count,W=perceptron_learning(X_train,Y_train)
 	Y_pred=predict(X_test,W)
 	disagreement=1-accuracy(Y_test,Y_pred)
 	return (iter_count, disagreement)
 
 def main(tests, sample_size, sample_tests=100):
+	'''
+	train perceptron using sample_size points, test sample_tests*tests times
+	output average number of iterations, average disagreement with target function
+	'''
 	iteration_counts = []
 	disagreements = []
 	for t in range(1,tests+1):
